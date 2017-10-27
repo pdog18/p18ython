2877e0ddd08e70caf212c5960032f2da950e7e84
pdog <pdog@qq.com>
2017-10-27 16:46:47 +0800

    change readme.md

--- add_wordlist.py
+++ add_wordlist.py
@@ -29,7 +29,6 @@ def add_wordlist(id,word):
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Host': 'www.shanbay.com',
         'Origin': 'https://www.shanbay.com',
-        'Referer': 'https://www.shanbay.com/wordlist/195865/585211/',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest',
         'Cookie': cookie
